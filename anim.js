d18adad1d594bbca56fbdf83d0e2a8704dd4e7f4
PutterZORDOR <132139589+PutterZORDOR@users.noreply.github.com>
2024-09-29 22:58:47 +0700

    qwerty

--- anim.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('navigateButton').addEventListener('click', function() {
-    // ใช้ window.location.href เพื่อไปยัง URL ใหม่
-    window.location.href = 'next-page.html';  // URL ของหน้าที่ต้องการไป
-});
