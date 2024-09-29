1971507020abd9442365f89e8aefb5ff9282706d
PutterZORDOR <132139589+PutterZORDOR@users.noreply.github.com>
2024-09-29 22:56:37 +0700

    picuter2

---
+++ js/anim.js
@@ -0,0 +1,4 @@
+document.getElementById('navigateButton').addEventListener('click', function() {
+    // ใช้ window.location.href เพื่อไปยัง URL ใหม่
+    window.location.href = 'next-page.html';  // URL ของหน้าที่ต้องการไป
+});
